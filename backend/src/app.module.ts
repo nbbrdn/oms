9e4926e5d30ae17e32f33abe76f8c00303b42df9
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-09-30 23:51:33 +1000

    Implement the catalog api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { GroupModule } from './group/group.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user',
-      password: 'password',
-      database: 'oms',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     GroupModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
---
+++ backend/src/product/product.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.entity';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.productService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() product: Product) {
+    return this.productService.create(product);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() product: Partial<Product>) {
+    return this.productService.update(id, product);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.productService.remove(id);
+  }
+}
